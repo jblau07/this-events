fd4c464004c833cc8cf91c99a3d3a779ef2e5f34
Justin Lau <jlau7248@gmail.com>
2017-07-25 21:49:26 -1000

    added showItem function

--- js/app.js
+++ js/app.js
@@ -2,3 +2,18 @@
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
+
+
+var hElem = document.getElementsByClassName("name");
+for(var i = 0; i<hElem.length; i++){
+	hElem[i].addEventListener("click", showItem);
+}
+function showItem(){
+  var priceElem = this.querySelector(".menu");
+  if(priceElem.style.display === "none"){
+    priceElem.style.display = "block";
+  }else{
+    priceElem.style.display = "none";
+  }
+  }
+
